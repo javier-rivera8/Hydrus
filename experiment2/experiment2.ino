20398f685c230a2e0d1b2eef15a3e60d99a42c1c
elo356 <elian.0617@gmail.com>
2025-08-14 02:04:46 -0400

    move 2 motors

--- experiment2/experiment2.ino
+++ experiment2/experiment2.ino
@@ -1,9 +1,7 @@
 #include <Servo.h>
 
-// Declaración de motores
 Servo motor1, motor2, motor3, motor4, motor5, motor7, motor8;
 
-// Pines de cada motor
 int pinMotor1 = 9;
 int pinMotor2 = 3;
 int pinMotor3 = 4;
@@ -11,6 +9,16 @@ int pinMotor4 = 5;
 int pinMotor5 = 6;
 int pinMotor7 = 8;
 int pinMotor8 = 2;
+
+void moverMotor(int motorNum, int señal) {
+  if (motorNum == 1) motor1.writeMicroseconds(señal);
+  else if (motorNum == 2) motor2.writeMicroseconds(señal);
+  else if (motorNum == 3) motor3.writeMicroseconds(señal);
+  else if (motorNum == 4) motor4.writeMicroseconds(señal);
+  else if (motorNum == 5) motor5.writeMicroseconds(señal);
+  else if (motorNum == 7) motor7.writeMicroseconds(señal);
+  else if (motorNum == 8) motor8.writeMicroseconds(señal);
+}
 
 void setup() {
   motor1.attach(pinMotor1);
@@ -23,14 +31,13 @@ void setup() {
 
   Serial.begin(9600);
 
-  // Inicializamos todos en stop
-  motor1.writeMicroseconds(1500);
-  motor2.writeMicroseconds(1500);
-  motor3.writeMicroseconds(1500);
-  motor4.writeMicroseconds(1500);
-  motor5.writeMicroseconds(1500);
-  motor7.writeMicroseconds(1500);
-  motor8.writeMicroseconds(1500);
+  moverMotor(1, 1500);
+  moverMotor(2, 1500);
+  moverMotor(3, 1500);
+  moverMotor(4, 1500);
+  moverMotor(5, 1500);
+  moverMotor(7, 1500);
+  moverMotor(8, 1500);
 
   Serial.println("Todos los motores inicializados en stop");
   delay(2000);
@@ -43,78 +50,35 @@ void loop() {
 
     int firstSpace = command.indexOf(' ');
     int secondSpace = command.indexOf(' ', firstSpace + 1);
+    int thirdSpace = command.indexOf(' ', secondSpace + 1);
 
-    if (command.startsWith("forward") && firstSpace > 0 && secondSpace > firstSpace) {
-      int motorNum = command.substring(firstSpace + 1, secondSpace).toInt();
-      int duration = command.substring(secondSpace + 1).toInt();
+    if ((command.startsWith("forward") || command.startsWith("backward")) &&
+        firstSpace > 0 && secondSpace > firstSpace && thirdSpace > secondSpace) {
 
-      if (duration > 0) {
-        Serial.println("Motor " + String(motorNum) + " forward por " + String(duration) + " segundos");
+      int motorA = command.substring(firstSpace + 1, secondSpace).toInt();
+      int motorB = command.substring(secondSpace + 1, thirdSpace).toInt();
+      int duration = command.substring(thirdSpace + 1).toInt();
 
-        if (motorNum == 1) motor1.writeMicroseconds(1600);
-        else if (motorNum == 2) motor2.writeMicroseconds(1600);
-        else if (motorNum == 3) motor3.writeMicroseconds(1600);
-        else if (motorNum == 4) motor4.writeMicroseconds(1600);
-        else if (motorNum == 5) motor5.writeMicroseconds(1600);
-        else if (motorNum == 7) motor7.writeMicroseconds(1600);
-        else if (motorNum == 8) motor8.writeMicroseconds(1600);
+      int señal = command.startsWith("forward") ? 1600 : 1400;
 
-        delay(duration * 1000);
+      Serial.println("Moviendo motores " + String(motorA) + " y " + String(motorB) +
+                     (señal==1600 ? " forward " : " backward ") + String(duration) + " segundos");
 
-        if (motorNum == 1) motor1.writeMicroseconds(1500);
-        else if (motorNum == 2) motor2.writeMicroseconds(1500);
-        else if (motorNum == 3) motor3.writeMicroseconds(1500);
-        else if (motorNum == 4) motor4.writeMicroseconds(1500);
-        else if (motorNum == 5) motor5.writeMicroseconds(1500);
-        else if (motorNum == 7) motor7.writeMicroseconds(1500);
-        else if (motorNum == 8) motor8.writeMicroseconds(1500);
+      moverMotor(motorA, señal);
+      moverMotor(motorB, señal);
 
-        Serial.println("Motor " + String(motorNum) + " detenido");
-      }
+      delay(duration * 1000);
+
+      moverMotor(motorA, 1500);
+      moverMotor(motorB, 1500);
+
+      Serial.println("Motores detenidos");
     }
 
-    // Comando: backward <motor> <duración>
-    else if (command.startsWith("backward") && firstSpace > 0 && secondSpace > firstSpace) {
-      int motorNum = command.substring(firstSpace + 1, secondSpace).toInt();
-      int duration = command.substring(secondSpace + 1).toInt();
-
-      if (duration > 0) {
-        Serial.println("Motor " + String(motorNum) + " backward por " + String(duration) + " segundos");
-
-        if (motorNum == 1) motor1.writeMicroseconds(1400);
-        else if (motorNum == 2) motor2.writeMicroseconds(1400);
-        else if (motorNum == 3) motor3.writeMicroseconds(1400);
-        else if (motorNum == 4) motor4.writeMicroseconds(1400);
-        else if (motorNum == 5) motor5.writeMicroseconds(1400);
-        else if (motorNum == 7) motor7.writeMicroseconds(1400);
-        else if (motorNum == 8) motor8.writeMicroseconds(1400);
-
-        delay(duration * 1000);
-
-        if (motorNum == 1) motor1.writeMicroseconds(1500);
-        else if (motorNum == 2) motor2.writeMicroseconds(1500);
-        else if (motorNum == 3) motor3.writeMicroseconds(1500);
-        else if (motorNum == 4) motor4.writeMicroseconds(1500);
-        else if (motorNum == 5) motor5.writeMicroseconds(1500);
-        else if (motorNum == 7) motor7.writeMicroseconds(1500);
-        else if (motorNum == 8) motor8.writeMicroseconds(1500);
-
-        Serial.println("Motor " + String(motorNum) + " detenido");
-      }
-    }
-
-    // Comando: stop <motor>
+    // Comando stop <motor>
     else if (command.startsWith("stop")) {
       int motorNum = command.substring(5).toInt();
-
-      if (motorNum == 1) motor1.writeMicroseconds(1500);
-      else if (motorNum == 2) motor2.writeMicroseconds(1500);
-      else if (motorNum == 3) motor3.writeMicroseconds(1500);
-      else if (motorNum == 4) motor4.writeMicroseconds(1500);
-      else if (motorNum == 5) motor5.writeMicroseconds(1500);
-      else if (motorNum == 7) motor7.writeMicroseconds(1500);
-      else if (motorNum == 8) motor8.writeMicroseconds(1500);
-
+      moverMotor(motorNum, 1500);
       Serial.println("Motor " + String(motorNum) + " detenido manualmente");
     }
   }
